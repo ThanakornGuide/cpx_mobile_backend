b54b6cb94f8181795a0325a694a5e261de733a85
ThanakornGuide <thanakornguide@gmail.com>
2022-06-20 16:34:23 +0700

    first commit

---
+++ src/main/java/com/demo/cpx_mobile_backend/CpxMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cpx_mobile_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpxMobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpxMobileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/cpx_mobile_backend/CpxMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cpx_mobile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpxMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
