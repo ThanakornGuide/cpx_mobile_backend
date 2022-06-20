324881a6ca07890d172550e00618a03624fe56ef
ThanakornGuide <thanakornguide@gmail.com>
2022-06-20 18:18:43 +0700

    q2

--- src/main/java/com/demo/cpx_mobile_backend/Controller/testoutput.java
+++ src/main/java/com/demo/cpx_mobile_backend/Controller/testoutput.java
@@ -1,4 +1,4 @@
-package Controller;
+package com.demo.cpx_mobile_backend.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testoutput {
     
     @GetMapping("/test_output")
-    public String demo (){
-        return "Hello Backemd Team";
+    public String demo() {
+        return "Hello Backend Team";
     }
 }
--- src/main/java/com/demo/cpx_mobile_backend/CpxMobileBackendApplication.java
+++ src/main/java/com/demo/cpx_mobile_backend/CpxMobileBackendApplication.java
@@ -2,12 +2,23 @@ package com.demo.cpx_mobile_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.cpx_mobile_backend.Controller.testoutput;
 
 @SpringBootApplication
+@RestController
 public class CpxMobileBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CpxMobileBackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
